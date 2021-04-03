2cb7942c619a7c6fb4ec4202fee5f8184423ee3b
qiuxe <qiuxe@yahoo.com>
2021-04-03 16:17:49 -0700

    add test cases and logging

--- src/main/java/sparkml/inference/SparkMLModelLoader.java
+++ src/main/java/sparkml/inference/SparkMLModelLoader.java
@@ -10,34 +10,45 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SparkMLModelLoader {
+    private final static Logger logger = LoggerFactory.getLogger(SparkMLModelLoader.class);
+
     private static String getModelClassName(String path) throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
 
         // TODO: what if multiple path-xxxxx in the path ?
         JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(path + "/metadata/part-00000"));
-
         String className = (String) jsonObject.get("class");
-        System.out.println("className: " + className);
+        logger.info("Model Class Name: " + className);
         return className;
     }
 
+    /**
+     * load a binary classification model from path
+     */
     public static <FeaturesType, M extends ProbabilisticClassificationModel<FeaturesType, M>>
     ProbabilisticClassificationModel<FeaturesType, M> loadProbabilisticClassificationModel(String path) {
 
         ProbabilisticClassificationModel<FeaturesType, M> model = null;
         try {
+            logger.info("starting spark instance");
             SparkSession spark = SparkSession.builder().master("local[1]").getOrCreate();
-
+            spark.sparkContext().setLogLevel("WARN");
             String className = getModelClassName(path);
             Method m = Class.forName(className).getMethod("load", String.class);
             Object modelObj = m.invoke(null, path); // load is a static method, no object is required to invoke
 
             model = (ProbabilisticClassificationModel<FeaturesType, M>) modelObj;
 
+            logger.info("load model successfully");
+            logger.info("number of features: " + model.numFeatures());
+            logger.info("stopping spark instance");
             spark.stop();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("failed to load model", e);
         }
         return model;
     }
--- src/test/java/sparkml/inference/SparkMLModelLoaderTest.java
+++ src/test/java/sparkml/inference/SparkMLModelLoaderTest.java
@@ -1,47 +1,76 @@
 package sparkml.inference;
+
 import org.apache.spark.SparkContext;
+import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.classification.LogisticRegressionModel;
 import org.apache.spark.ml.classification.ProbabilisticClassificationModel;
 import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.apache.spark.sql.RowFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
 
 public class SparkMLModelLoaderTest {
-    @Before
-    public void trainLogisticModel() {
+    private final static Logger logger = LoggerFactory.getLogger(SparkMLModelLoaderTest.class);
+    private static String modelPath = "build/lrModel";
+
+    @BeforeClass
+    public static void trainLogisticModel() throws IOException {
+        ClassLoader classLoader = SparkMLModelLoaderTest.class.getClassLoader();
+        // the data file is downloaded from spark's github project
+        String dataFilePath = classLoader.getResource("data/mllib/sample_libsvm_data.txt").getPath();
+        logger.info("training a logistic model from data: " + dataFilePath);
         SparkSession spark = SparkSession.builder().master("local[1]").getOrCreate();
         SparkContext sc = spark.sparkContext();
-        sc.setLogLevel("INFO");
-        Row row0 = RowFactory.create(1.0, Vectors.dense(1.0, 2.0));
-        Row row1 = RowFactory.create(0.0, Vectors.dense(-1.0, -2.0));
+        sc.setLogLevel("WARN");
+        // Load training data
+        Dataset<Row> training = spark.read().format("libsvm")
+                .load(dataFilePath);
 
-        // TODO: create sample data set and train a model
-        List<Row> list=new ArrayList<Row>();
-        list.add(row0);
-        list.add(row1);
-        //spark
-        //sc.parallelize(list, 1);
+
+        LogisticRegression lr = new LogisticRegression()
+                .setMaxIter(10)
+                .setRegParam(0.3)
+                .setElasticNetParam(0.8);
+
+        // Fit the model
+        LogisticRegressionModel lrModel = lr.fit(training);
+
+        // Print the coefficients and intercept for logistic regression
+        logger.info("Coefficients: " + lrModel.coefficients()
+                + " Intercept: " + lrModel.intercept());
+        lrModel.write().overwrite().save(modelPath);
         spark.stop();
     }
+
+
     @Test
     public void testLoadProbabilisticClassificationModel() {
-        ProbabilisticClassificationModel<Vector, LogisticRegressionModel> model = SparkMLModelLoader.loadProbabilisticClassificationModel("/Users/xiqiu/blor");
+        ProbabilisticClassificationModel<Vector, LogisticRegressionModel> model = SparkMLModelLoader.loadProbabilisticClassificationModel(modelPath);
+        int numFeatures = 692;
+        Assert.assertEquals(numFeatures, model.numFeatures());
+        Vector x = Vectors.sparse(numFeatures, new int[]{244 ,263}, new double[]{-1000.0, -2000.0});
 
-        double predLabel = model.predict(Vectors.dense(-1.1, 2.2));
+        Vector predProbs = model.predictProbability(x);
+        logger.info("prediction predProbs: " + predProbs);
+
+        double predLabel = model.predict(x);
+        logger.info("prediction label: " + predLabel);
         assertTrue(predLabel > 0);
 
-        Vector predProbs = model.predictProbability(Vectors.dense(-1.1, 2.2));
-        System.out.println("prediction label: " + predLabel);
-        System.out.println("prediction predProbs: " + predProbs);
-
+        model.setThresholds(new double[] {0.1, 0.9});
+        logger.info("new threshold: " + model.getThresholds()[1]);
+        assertTrue(model.predict(x) < 1);
     }
 
 
